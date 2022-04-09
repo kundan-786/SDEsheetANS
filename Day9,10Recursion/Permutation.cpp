23bc274cb8d69f0b2668372d0d000c93f5987459
Kundan Giri <58781808+kundan-786@users.noreply.github.com>
2022-04-09 15:39:10 +0530

    Update Permutation.cpp

--- Day9,10Recursion/Permutation.cpp
+++ Day9,10Recursion/Permutation.cpp
@@ -1,22 +1,46 @@
-class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-          vector<vector<int>> ans;
-        recurPer(0,nums,ans);
+//TC: O(N!) * O(N) 
+ // SC: O(N) + O(N)
+  
+  vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<int> mp(nums.size(),0);
+        func(ds,ans,mp,nums);
         return ans;
     }
-    
-    
-  void recurPer(int idx,vector<int>& nums, vector<vector<int>>& ans){
-        if(idx==nums.size()-1)
-        {ans.push_back(nums);
-        return;}
-        
-      for(int i=idx;i<nums.size();i++){
-          swap(nums[idx],nums[i]);
-          recurPer(idx+1,nums,ans);
-          swap(nums[idx],nums[i]);
-      }
-        
+    void func(vector<int>&ds,vector<vector<int>>&ans,vector<int>&mp,vector<int>&nums){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!mp[i]){
+                mp[i] = 1;
+                ds.push_back(nums[i]);
+                func(ds,ans,mp,nums);
+                ds.pop_back();
+                mp[i] = 0; 
+            }
+        }
     }
-};
+
+       ---------OR---------
+ // TC: O(N!) * O(N)
+ // SC: auxilary space
+  
+      vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        func(0,ans,nums);
+        return ans;
+    }
+    void func(int index,vector<vector<int>>&ans,vector<int>&nums){
+        if(index==nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]); //swapping for every position
+            func(index+1,ans,nums);
+            swap(nums[index],nums[i]); //reswap
+        }
+    }
